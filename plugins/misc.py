ac598f22fa098e7ff45ac1f9289b20a2bee031e5
Vishal Mehta <59087756+RdxTv@users.noreply.github.com>
2022-08-11 18:13:43 +0530

    Update misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -20,7 +20,7 @@ async def showid(client, message):
         username = message.from_user.username
         dc_id = message.from_user.dc_id or ""
         await message.reply_text(
-            f"<b>🦹 First Name:</b> {first}\n<b>🚣 Last Name:</b> {last}\n<b> Username:</b>👻 {username}\n<b>🆔 Telegram ID:</b> <code>{user_id}</code>\n<b>🏢 Data Centre:</b> <code>{dc_id}</code>",
+            f"<b>🦹 First Name:</b> {first}\n<b>🪰 Last Name:</b> {last}\n<b> Username:</b>👻 {username}\n<b>🆔 Telegram ID:</b> <code>{user_id}</code>\n<b>🏢 Data Centre:</b> <code>{dc_id}</code>",
             quote=True
         )
 
@@ -75,7 +75,7 @@ async def who_is(client, message):
     message_out_str = ""
     message_out_str += f"<b>🦹First Name:</b> {from_user.first_name}\n"
     last_name = from_user.last_name or "<b>None</b>"
-    message_out_str += f"<b>🚣Last Name:</b> {last_name}\n"
+    message_out_str += f"<b>🪰Last Name:</b> {last_name}\n"
     message_out_str += f"<b>🆔Telegram ID:</b> <code>{from_user.id}</code>\n"
     username = from_user.username or "<b>None</b>"
     dc_id = from_user.dc_id or "[User Doesn't Have A Valid DP]"
